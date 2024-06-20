7bc3d7c08641792fc2d3fe7681fcc626b7a9f2a6
YashrajKamble <yashrajkamble7727@gmail.com>
2024-06-20 19:31:29 +0530

    some changes in todoslice.js

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-  todos: [{ id: 1, text: "Hello World" }],
+  todos: [{ id: 1, text: "Hello world" }],
 };
 
 export const todoSlice = createSlice({
@@ -11,17 +11,16 @@ export const todoSlice = createSlice({
     addTodo: (state, action) => {
       const todo = {
         id: nanoid(),
-        text: action.payload.text,
+        text: action.payload,
       };
       state.todos.push(todo);
     },
     removeTodo: (state, action) => {
-      state.todos = state.filter((todo) => todo.id !== action.payload);
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
     },
-    // updatesTodo: () => {},
   },
 });
 
 export const { addTodo, removeTodo } = todoSlice.actions;
 
-export default todoSlice.reducer
+export default todoSlice.reducer;
